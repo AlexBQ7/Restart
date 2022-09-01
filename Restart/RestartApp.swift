32726ea4cc0fb5eed187b8c319f89b8beb639e64
AlexBQ7 <alejandro.barreto.quintana@gmail.com>
2022-08-31 21:45:04 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by David Alejandro Ruiz Hernandez on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
